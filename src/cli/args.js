899bc165d5e85bb32d6d45b01c1ded6fd291e0a5
Kambar <kidkambar@gmail.com>
2024-10-01 14:48:45 +0500

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2)
+  let res = ''
+  const arr = []
+  for(let i = 0; i < args.length; i += 2) {
+    arr.push(args.slice(i, i + 2))
+  }
+  for(let i = 0; i < arr.length; i++) {
+    if(i !== arr.length - 1) {
+      res += `${arr[i][0].slice(2)} is ${arr[i][1]}, `
+    } else {
+      res += `${arr[i][0].slice(2)} is ${arr[i][1]}`
+    }
+  }
+  console.log(res)
 };
 
 parseArgs();
