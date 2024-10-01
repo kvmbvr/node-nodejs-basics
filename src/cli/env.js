d5e3a52a4a9ea19a0039b1657afb8b626caca7c9
Kambar <kidkambar@gmail.com>
2024-10-01 15:17:42 +0500

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const obj = process.env
+  const keys = Object.keys(obj).filter(key => key.startsWith('RSS_'))
+  let res = ''
+  for (let i = 0; i < keys.length; i++) {
+    if(i !== keys.length - 1) {
+      res += `${keys[i]}=${obj[keys[i]]}; `
+    } else {
+      res += `${keys[i]}=${obj[keys[i]]}`
+    }
+  }
+  console.log(res)
 };
 
 parseEnv();
