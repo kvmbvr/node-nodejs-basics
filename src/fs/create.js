062c8a7da6a1805b82eec097e4d9d0a5b422b7c8
Kambar <kidkambar@gmail.com>
2024-10-03 00:19:32 +0500

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs, { access } from 'fs'
+import { constants } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const create = async () => {
-    // Write your code here 
+  fs.open(path.join(__dirname, 'files/fresh.txt'), 'ax+', (err) => {
+    if(err) {
+      if(err.code === 'EEXIST') {
+        throw new Error('FS operation failed');
+      }
+    }
+  })
+
+  fs.writeFile(path.join(__dirname, 'files/fresh.txt'), 'I am fresh and young', 'utf8', (err) => {
+    if(err) {
+      console.err(err.message)
+    }
+  })
 };
 
 await create();
