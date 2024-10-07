068b297a11e1b67f87178b0b3e200c0db4ddbf6b
Kambar <kidkambar@gmail.com>
2024-10-08 01:12:10 +0500

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fs from 'fs/promises'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const wrongFilePath = path.join(__dirname, 'files/wrongFilename.txt');
+const properFilePath = path.join(__dirname, 'files/properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.access(wrongFilePath)
+    try {
+      await fs.access(properFilePath)
+      throw Error('FS operation failed properfile')
+    } catch(err) {
+      if(err){
+        if(err.code === 'ENOENT') {
+          await fs.rename(wrongFilePath, properFilePath);
+        } 
+      }
+    }
+  } catch(err) {
+    if(err) {
+      throw new Error('FS operation failed wrongfile')
+    }
+  }
 };
 
 await rename();
